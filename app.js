7a7a95e66ed0cf1139a90d2d7275fd7e030cf157
tahahhany11192 <tahah8833@gmail.com>
2026-07-04 21:55:59 +0300

    Change server listen address to 0.0.0.0

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log("Server is running on", PORT);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on 0.0.0.0:${PORT}`);
 });
